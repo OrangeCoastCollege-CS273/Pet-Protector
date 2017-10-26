3fc102e260cb7289570119ff509dc373acf3034d
sbadajoz <sbadajoz@student.cccd.edu>
2017-10-26 14:57:07 -0700

    Create method getUriFromResource

--- app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListActivity.java
@@ -1,7 +1,11 @@
 package edu.orangecoastcollege.cs273.petprotector;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class PetListActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class PetListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_list);
+    }
+
+    public static Uri getUriFromResource(Context context, int resID) {
+        Resources res = context.getResources();
+        String uri = ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+                + res.getResourcePackageName(resID) + "/"
+                + res.getResourceTypeName(resID) + "/"
+                + res.getResourceEntryName(resID);
+        return Uri.parse(uri);
     }
 }
