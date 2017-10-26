34a061e764c6b1517e748b8529d1a52783705834
sbadajoz <sbadajoz@student.cccd.edu>
2017-10-26 16:19:45 -0700

    Complete functionality, add permission requests, gallery launch and image fetch

--- app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListActivity.java
@@ -1,15 +1,29 @@
 package edu.orangecoastcollege.cs273.petprotector;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PetListActivity extends AppCompatActivity {
     private ImageView petImageView;
+    private Uri petImageUri;
+
+    public static final int GRANTED = PackageManager.PERMISSION_GRANTED;
+    public static final int DENIED = PackageManager.PERMISSION_DENIED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +34,45 @@ public class PetListActivity extends AppCompatActivity {
         petImageView.setImageURI(getUriFromResource(this, R.drawable.none));
     }
 
+    public void selectPetImage(View view) {
+        List<String> permsList = new ArrayList<>();
 
+        int hasCameraPerm = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (hasCameraPerm == DENIED)
+            permsList.add(Manifest.permission.CAMERA);
+
+        int hasReadPerm = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (hasReadPerm == DENIED)
+            permsList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        int hasWritePerm = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (hasWritePerm == DENIED)
+            permsList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+
+        if (permsList.size() > 0) {
+            String[] permsArray = new String[permsList.size()];
+            permsList.toArray(permsArray);
+            ActivityCompat.requestPermissions(this,permsArray, 1337);
+        }
+
+        if (hasCameraPerm == GRANTED && hasReadPerm == GRANTED && hasWritePerm == GRANTED) {
+            Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(gallery, 1);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case(1):
+                if (resultCode == RESULT_OK && data != null) {
+                    petImageUri = data.getData();
+                    petImageView.setImageURI(petImageUri);
+                }
+        }
+    }
 
     public static Uri getUriFromResource(Context context, int resID) {
         Resources res = context.getResources();
