033a18529df585cd87f91e08e659a4ca76fa50f6
sbadajoz <sbadajoz@student.cccd.edu>
2017-10-26 15:34:45 -0700

    Connect ImageVIew to layout

--- app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListActivity.java
@@ -6,14 +6,21 @@ import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class PetListActivity extends AppCompatActivity {
+    private ImageView petImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_list);
+
+        petImageView = (ImageView) findViewById(R.id.petImageView);
+        petImageView.setImageURI(getUriFromResource(this, R.drawable.none));
     }
+
+
 
     public static Uri getUriFromResource(Context context, int resID) {
         Resources res = context.getResources();
