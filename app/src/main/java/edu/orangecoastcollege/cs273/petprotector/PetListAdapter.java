4925dd55932341264e891cf12984a03dff0387ec
Severiano Badajoz <sevebadajoz@gmail.com>
2017-11-07 13:08:36 -0800

    Add functionality

--- app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListAdapter.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/petprotector/PetListAdapter.java
@@ -1,7 +1,52 @@
 package edu.orangecoastcollege.cs273.petprotector;
 
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PetListAdapter extends ArrayAdapter {
+    private Context mContext;
+    private  int mResourceID;
+    private List<Pet> mPetList = new ArrayList<>();
+
+    public PetListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Pet> pets) {
+        super(context, resource, pets);
+        mContext = context;
+        mResourceID = resource;
+        mPetList = pets;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(mResourceID, null);
+
+        LinearLayout petListLinearLayout = (LinearLayout) view.findViewById(R.id.petListLinearLayout);
+        ImageView petListImageView = (ImageView) view.findViewById(R.id.petListImageView);
+        TextView petListNameTextView = (TextView) view.findViewById(R.id.petListNameTextView);
+        TextView petListDetailsTextView = (TextView) view.findViewById(R.id.petListDetailsTextView);
+
+        Pet pet = mPetList.get(position);
+        petListImageView.setImageURI(pet.getImage());
+        petListNameTextView.setText(pet.getName());
+        petListDetailsTextView.setText(pet.getDetails());
+
+        petListLinearLayout.setTag(pet);
+
+        return view;
+    }
 }
